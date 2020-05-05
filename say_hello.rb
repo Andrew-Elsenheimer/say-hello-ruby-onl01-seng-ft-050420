28aaefc027e44f46c730796f375a57b7db24fddd
Andrew Elsenheimer <drew.elsenheimer1@yahoo.com>
2020-05-05 04:58:07 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,13 +1,10 @@
 # Build your say_hello method here
 
 
-
-#def say_hello(name)
-#  puts "Hello #{name}!"
-#end
-
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!""
 end
 
-("Kent Beck")
+
+
+say_hello("Kent Beck")
