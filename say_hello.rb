ddf47343f30bfd63ae24b214073f94bec9e4037f
Andrew Elsenheimer <drew.elsenheimer1@yahoo.com>
2020-05-05 04:50:55 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -6,8 +6,7 @@
 #  puts "Hello #{name}!"
 #end
 
-def say_hello(name)
-  puts "Hello Ruby Programmer!"
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!""
 end
 
-# say_hello(Kent Beck)
